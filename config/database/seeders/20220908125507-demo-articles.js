ee1911ba7a88b097b03a085ea8c0be2e5f3e2e04
Marcoff90 <marcoff90@icloud.com>
2022-09-20 13:49:36 +0200

    Fix comments service to return comment without published value, update seeders to seed comments for all articles

--- config/database/seeders/20220908125507-demo-articles.js
+++ config/database/seeders/20220908125507-demo-articles.js
@@ -3,25 +3,95 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     const articles = [];
-    for (let i = 0; i < 5; i++) {
-      articles.push({
-        title: `Lorem ipsum${i + 1}`,
-        perex: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse nisl. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
-        content: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse nisl. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Duis condimentum augue id magna semper rutrum. Nulla non arcu lacinia neque faucibus fringilla. Etiam bibendum elit eget erat. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Donec quis nibh at felis congue commodo. Vivamus ac leo pretium faucibus. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Maecenas aliquet accumsan leo. In enim a arcu imperdiet malesuada.
+    for (let i = 1; i < 6; i++) {
+      for (let j = 1; j < 6; j++) {
+        articles.push({
+          title: `Lorem ipsum${i * j}`,
+          perex: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse nisl. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
+          content: `# Veri virginitas quanta commemorat vestras
 
-Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Integer in sapien. Nulla pulvinar eleifend sem. Pellentesque ipsum. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Nunc auctor. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. In rutrum. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla pulvinar eleifend sem. Sed ac dolor sit amet purus malesuada congue. Duis risus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Integer malesuada.${i + 1}`,
-        deleted: false,
-        state: `done`,
-        image: `../../../assets/article-images/image${i + 1}.jpeg`,
-        user_id: i + 1
-      });
+## Tuto herbas
+
+Lorem **markdownum tantum**, tum et dixit ardebant **habitantem tempora potat**
+fertilitas. Strictique ignara Theseus auspicio Phrygii; adspicit scit patrem
+ordine materiamque epulae, iniqua, ita aera. Nisi est longi summis purasque
+cessit nitidaque alimenta et volvere manus iudice illi dieque spemque et.
+
+Ponti solidumve inde movit ante radiantis: nata candescere dedit ipse, iugalia
+mente! Duce talum litem pectus [praestatque condita
+siste](http://vellent.org/tuosaxo.html) nata esse totoque. Postquam quaerunt
+accepti stridore subito, mentis ab facies nec. Auxilio vates. Dea qua, sum
+*labant*; quo mala bella ferus vacet timet procumbere quoque prohibentque
+saturae tot agnovit.
+
+> Dixisse fieri, equo induruit fecit et quam generosaque sic obstructaque,
+> Hippomenes! Turba longoque iubet. Turbant pudor quo contra *sequendi quippe*,
+> axis erravit falsum? Ardore volenti frustraque litora releguntque Lycabas,
+> breve viros dabit caelestibus vices capillos, premit medullas in quod,
+> Hesperium. Saltem inmotas.
+
+## Senior tum rubefactaque prolisque nam modo Cebrenida
+
+Dextra in si namque partus, qui ut iuvabat at turres, Aiax dea sistitur silentia
+maxima parenti habebat. Aqua spinae vobis: nec neque Oceanum nox undas casias,
+ferit non, trux ceu, en. Novi excessisse circumdata ardet decertare corruit
+suoque avus modo inventa tuo *multo excipiunt* cetera iubes diverso, ille.
+Sustinet mox Capetusque duos conceptus coimus: in, deus es Clanis deprenduntur
+**saltibus repulsae ad** vires, Cydoneasque. Mite visaque non clamant erat nec
+Orphea sed sim aratri, et.
+
+Cernit instabat et tamen odoratis furiale sollicitive quo nunc segetes Europam
+eloquioque dant et saepe, deo veniat. Ipsa deque tunc ture morboque aequalibus
+accedere Peleus de mortales videndi imagine gelidis moenia. Nulla
+[Coae](http://umbraein.org/) pectora movere fera cultusque regnat cunctaque mox,
+moriere obsistere Clytumque cuncta, quae sola!`,
+          deleted: false,
+          state: `done`,
+          image: `../../../assets/article-images/image${i}.jpeg`,
+          user_id: i
+        });
+      }
     }
     const draft = {
       title: `Lorem ipsum`,
       perex: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse nisl. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`,
-      content: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Suspendisse nisl. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Duis condimentum augue id magna semper rutrum. Nulla non arcu lacinia neque faucibus fringilla. Etiam bibendum elit eget erat. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Donec quis nibh at felis congue commodo. Vivamus ac leo pretium faucibus. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Maecenas aliquet accumsan leo. In enim a arcu imperdiet malesuada.
+      content: `# Veri virginitas quanta commemorat vestras
 
-Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Integer in sapien. Nulla pulvinar eleifend sem. Pellentesque ipsum. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Nunc auctor. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. In rutrum. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla pulvinar eleifend sem. Sed ac dolor sit amet purus malesuada congue. Duis risus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Integer malesuada.`,
+## Tuto herbas
+
+Lorem **markdownum tantum**, tum et dixit ardebant **habitantem tempora potat**
+fertilitas. Strictique ignara Theseus auspicio Phrygii; adspicit scit patrem
+ordine materiamque epulae, iniqua, ita aera. Nisi est longi summis purasque
+cessit nitidaque alimenta et volvere manus iudice illi dieque spemque et.
+
+Ponti solidumve inde movit ante radiantis: nata candescere dedit ipse, iugalia
+mente! Duce talum litem pectus [praestatque condita
+siste](http://vellent.org/tuosaxo.html) nata esse totoque. Postquam quaerunt
+accepti stridore subito, mentis ab facies nec. Auxilio vates. Dea qua, sum
+*labant*; quo mala bella ferus vacet timet procumbere quoque prohibentque
+saturae tot agnovit.
+
+> Dixisse fieri, equo induruit fecit et quam generosaque sic obstructaque,
+> Hippomenes! Turba longoque iubet. Turbant pudor quo contra *sequendi quippe*,
+> axis erravit falsum? Ardore volenti frustraque litora releguntque Lycabas,
+> breve viros dabit caelestibus vices capillos, premit medullas in quod,
+> Hesperium. Saltem inmotas.
+
+## Senior tum rubefactaque prolisque nam modo Cebrenida
+
+Dextra in si namque partus, qui ut iuvabat at turres, Aiax dea sistitur silentia
+maxima parenti habebat. Aqua spinae vobis: nec neque Oceanum nox undas casias,
+ferit non, trux ceu, en. Novi excessisse circumdata ardet decertare corruit
+suoque avus modo inventa tuo *multo excipiunt* cetera iubes diverso, ille.
+Sustinet mox Capetusque duos conceptus coimus: in, deus es Clanis deprenduntur
+**saltibus repulsae ad** vires, Cydoneasque. Mite visaque non clamant erat nec
+Orphea sed sim aratri, et.
+
+Cernit instabat et tamen odoratis furiale sollicitive quo nunc segetes Europam
+eloquioque dant et saepe, deo veniat. Ipsa deque tunc ture morboque aequalibus
+accedere Peleus de mortales videndi imagine gelidis moenia. Nulla
+[Coae](http://umbraein.org/) pectora movere fera cultusque regnat cunctaque mox,
+moriere obsistere Clytumque cuncta, quae sola!`,
       deleted: false,
       state: `draft`,
       image: `../../../assets/article-images/image1.jpeg`,
